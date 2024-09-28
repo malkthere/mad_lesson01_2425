f6bcfc049d7de7ed44cd1a53313884fb61ce0c4e
mazin <malkthere@gmail.com>
2024-09-28 08:47:27 +0300

    lesson 02 a state change

--- lib/main.dart
+++ lib/main.dart
@@ -1,44 +1,45 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyInteractiveApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MyInteractiveApp extends StatefulWidget {
+  @override
+  _MyInteractiveAppState createState() => _MyInteractiveAppState();
+}
+
+class _MyInteractiveAppState extends State<MyInteractiveApp> {
+  String message = 'Welcome to Flutter!';
+  int counter = 0;
+
+  void updateMessage() {
+    setState(() {
+      message = 'Button Pressed!';
+      counter++;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'lesson 01',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'College of Computers, MAD-lesson1'),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'Salam, Guys!, this is Dr. Mazin Alkathiti',
-              style: TextStyle(fontSize: 24),
-            ),
-          ],
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Interactive UI'),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(message),
+              Text("$counter"),
+              SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: updateMessage,
+                child: Text('Press Me'),
+              ),
+            ],
+          ),
         ),
       ),
     );
