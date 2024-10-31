ac17bff97637560d062d6fa350e4f3bdf95f5ad2
mazin <malkthere@gmail.com>
2024-10-31 07:14:08 +0300

    lesson 05-e Handling User Input and Navigation-1 (Form- part-2 navigation)

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 //import 'dart:io';
 
 import 'package:flutter/material.dart';
-
+import 'package:mad_lesson1_2425/SecondScreen.dart';
 void main() {
   runApp(ProfileApp());
 }
@@ -12,87 +12,29 @@ class ProfileApp extends StatefulWidget {
 }
 
 class _ProfileAppState extends State<ProfileApp> {
-  final _formKey = GlobalKey<FormState>();
-  String _name = '';
-  String _email = '';
-  final TextEditingController _passwordController = TextEditingController();
-
-
-  void _submitForm() {
-    if (_formKey.currentState!.validate()) {
-      _formKey.currentState!.save();
-      print('Name: $_name, Email: $_email, Password : ${_passwordController.text}');
-    }
-  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(title: Text('Form Example')),
-        body: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Form(
-            key: _formKey,
-            child: Column(
-              children: [
-                TextFormField(
-                  decoration: InputDecoration(labelText: 'Name'),
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Please enter your name';
-                    }
-                    return null;
-                  },
-                  onSaved: (value) {
-                    _name = value!;
-                  },
-                ),
-                SizedBox(height: 16),
-                TextFormField(
-                  decoration: InputDecoration(labelText: 'Email'),
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Please enter your email';
-                    } else if (!RegExp(r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+").hasMatch(value!)) {
-                      return 'Please enter a valid email address';
-                    }
-                    return null;
-                  },
-                  onSaved: (value) {
-                    _email = value!;
-                  },
-                ),
-                TextFormField(
-                  controller: _passwordController,
-                  decoration: InputDecoration(labelText: 'Password'),
-                  obscureText: true,
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Please enter your password';
-                    }
-                    else if (value!.length<8) {
-                      return ' password should be at least 8 chars';
-                    }else if (!RegExp("[a-z]").hasMatch(value!)) {
-                      return ' password should at least one lowercase chars';
-                    }else if (!RegExp("[A-Z]").hasMatch(value!)) {
-                      return ' password should at least one uppercase chars';
-                    }else if (!RegExp("[!#\$%&'*+-/=?^_`{|}~@]").hasMatch(value!)) {
-                      return ' password should at least one symbol';
-                    }else if (!RegExp("[0-9]").hasMatch(value!)) {
-                      return ' password should at least one number';
-                    }
-                    return null;
-                  },
-                ),
-                SizedBox(height: 20),
-                ElevatedButton(
-                  onPressed: _submitForm,
-                  child: Text('Submit'),
-                ),
-              ],
-            ),
-          ),
+      home: FirstScreen(),
+    );
+  }
+}
+
+class FirstScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('First Screen')),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => SecondScreen()),
+            );
+          },
+          child: Text('Go to Second Screen'),
         ),
       ),
     );
