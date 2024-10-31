fd4291e086939c5356f8307b449cf4845227bf59
mazin <malkthere@gmail.com>
2024-10-31 07:26:32 +0300

    lesson 05-f Handling User Input and Navigation-1 (navigation) part2

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,6 @@ class ProfileApp extends StatefulWidget {
 
 
 class _ProfileAppState extends State<ProfileApp> {
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -23,19 +22,28 @@ class _ProfileAppState extends State<ProfileApp> {
 }
 
 class FirstScreen extends StatelessWidget {
+  TextEditingController Textcontroller=new TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('First Screen')),
       body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => SecondScreen()),
-            );
-          },
-          child: Text('Go to Second Screen'),
+        child: Column(
+          children: [
+            TextField(
+              controller: Textcontroller,
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => SecondScreen(data: Textcontroller.text)),
+                );
+              },
+              child: Text('Go to Second Screen'),
+            ),
+          ],
         ),
       ),
     );
