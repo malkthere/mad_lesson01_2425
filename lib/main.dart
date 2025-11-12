3461ace2c275b4b9e07b79af728a586b4c1eb70a
mazin <malkthere@gmail.com>
2025-11-12 06:40:16 +0300

    lesson 04- d-  First Example: Simple Custom Stateless Widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,65 +1,53 @@
 import 'package:flutter/material.dart';
+import 'UserInfoCard.dart'; // assume it is in a separate file
 
 void main() {
-  runApp(TodoApp());
+  runApp(const MyApp());
 }
 
-class TodoApp extends StatefulWidget {
-  @override
-  _TodoAppState createState() => _TodoAppState();
-}
-
-class _TodoAppState extends State<TodoApp> {
-  final List<String> _todos = [];
-
-  void _addTodoItem(String task) {
-    if (task.isNotEmpty) {
-      setState(() {
-        _todos.add(task);
-      });
-    }
-  }
-
-  void _removeTodoItem(int index) {
-    setState(() {
-      _todos.removeAt(index);
-    });
-  }
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Simple To-Do App'),
-        ),
-        body: Column(
-          children: <Widget>[
-            TextField(
-              onSubmitted: _addTodoItem,
-              decoration: InputDecoration(
-                labelText: 'Enter a new task',
-              ),
-            ),
-            Expanded(
-              child: ListView.builder(
-                itemCount: _todos.length,
-                itemBuilder: (context, index) {
-                  return ListTile(
-                    title: Text(_todos[index]),
-                    //subtitle: Text("$index"),
-                    trailing: IconButton(
-                      icon: Icon(Icons.delete),
-                      onPressed: () => _removeTodoItem(index),
-                    ),
-                  );
-                },
-              ),
-            ),
-          ],
-        ),
-      ),
+      title: 'Custom Widget Demo',
+      home: const HomeScreen(),
     );
   }
 }
 
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Students List')),
+      body: ListView(
+        children: const [
+          UserInfoCard(
+            name: 'Ali Ahmed',
+            email: 'ali@example.com',
+            role: 'Student',
+          ),
+          UserInfoCard(
+            name: 'Sara Hassan',
+            email: 'sara@example.com',
+            role: 'Student',
+          ),
+          UserInfoCard(
+            name: 'Dr. Mazin',
+            email: 'teacher@example.com',
+            role: 'Teacher',
+          ),
+          UserInfoCard(
+            name: 'Dr. Mazin',
+            email: 'teacher@example.com',
+            role: 'Teacher',
+          ),
+        ],
+      ),
+    );
+  }
+}
